a3db670fcc7a1dce84f7473918fbefa181dc1e75
lab6601 <lab6601@caelum144-18>
2017-01-31 22:05:28 -0200

    correção

--- fj22-argentum/src/test/java/br/com/caelum/argentum/modelo/CandlestickFactoryTest.java
+++ fj22-argentum/src/test/java/br/com/caelum/argentum/modelo/CandlestickFactoryTest.java
@@ -27,6 +27,35 @@ public class CandlestickFactoryTest {
 		Assert.assertEquals(42.3, candle.Fechamento(), 0.00001);
 		Assert.assertEquals(39.8, candle.Minimo(), 0.00001);
 		Assert.assertEquals(45.0, candle.Maximo(), 0.00001);
-		Assert.assertEquals(1676.0, candle.Volume(), 0.00001);
+		Assert.assertEquals(16760.0, candle.Volume(), 0.00001);
+	}
+	@Test
+	public void semNegociacoesGeraCandleComZeros() {
+	  Calendar hoje = Calendar.getInstance();
+	  
+	  List<Negociacao> negociacoes = Arrays.asList(); 
+	    
+	  CandlestickFactory fabrica = new CandlestickFactory();
+	  Candlestick candle = fabrica.constroiCandleParaData(hoje, negociacoes);
+	  
+	  Assert.assertEquals(0.0, candle.Volume(), 0.00001);
+	}
+	
+	@Test
+	public void apenasUmaNegociacaoGeraCandleComValoresIguais() {
+	  Calendar hoje = Calendar.getInstance();
+	  Negociacao negociacao1 = new Negociacao(40.5, 100, hoje);
+
+	  List<Negociacao> negociacoes = Arrays.asList(negociacao1);
+
+	  CandlestickFactory fabrica = new CandlestickFactory();
+	  Candlestick candle = fabrica.constroiCandleParaData(hoje, negociacoes);
+
+	  Assert.assertEquals(40.5, candle.Abertura(), 0.00001);
+	  Assert.assertEquals(40.5, candle.Fechamento(), 0.00001);
+	  Assert.assertEquals(40.5, candle.Minimo(), 0.00001);
+	  Assert.assertEquals(40.5, candle.Maximo(), 0.00001);
+	  Assert.assertEquals(4050.0, candle.Volume(), 0.00001);
 	}
 }
+
